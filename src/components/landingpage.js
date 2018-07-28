577e59b215c05a1540d39b681724111b85228cd2
wasifmiraz <wasifmiraz@mail.com>
2018-07-28 21:57:32 +0600

    landing page up

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -8,10 +8,18 @@ class LandingPage extends Component {
             <Grid className="landing-grid">
             <Cell col={12}>
          <img src="https://www.shareicon.net/download/2015/09/18/103161_man_512x512.png" className="avatar-img" />
-  <div className="banner-text">
-<h1>Full Stack Developer</h1>
+     <div className="banner-text">
+
+  <h1>Full Stack Developer</h1>
 <hr/>
  <p>HTML/CSS| Bootstrap | Javascript | Node Js |PHP| React | Express</p>
+ <div className="social-link">
+ <a href="" target="_blink">
+<i ></i>
+
+ </a>
+
+ </div>
   </div>
             </Cell>
             </Grid>
