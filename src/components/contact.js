67d495c3c1a7a04eebf67767e46fb5112ee807f7
StreetRacer <38099690+streetracer48@users.noreply.github.com>
2019-05-04 11:37:43 +0600

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,7 +7,7 @@ class Contact extends Component{
 <div className="contact-body">
 <Grid className="contact-grid">
           <Cell col={6}>
-            <h2>Wasif Miraz</h2>
+            <h2>Streetracer</h2>
             <img
               src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
               alt="avatar"
